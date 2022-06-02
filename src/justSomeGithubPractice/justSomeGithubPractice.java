d58d2f5bd339d6fc695cf0080c56a9d0b1823b2f
jasonjkaminsky <jasonjkaminsky@gmail.com>
2022-06-01 20:31:51 -0500

    modified file

--- src/justSomeGithubPractice/justSomeGithubPractice.java
+++ src/justSomeGithubPractice/justSomeGithubPractice.java
@@ -5,6 +5,7 @@ public class justSomeGithubPractice {
 	public static void main(String[] args) {
 		
 		System.out.println("This is only for GitHub practice");
+		System.out.println("Modifying file to practice GitHub more");
 		
 
 	}
