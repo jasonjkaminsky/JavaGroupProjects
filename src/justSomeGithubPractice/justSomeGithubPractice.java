f856394dedfb42c7300eee6134a915d36aba7177
jasonjkaminsky <jasonjkaminsky@gmail.com>
2022-06-01 20:19:19 -0500

    fourth commit

---
+++ src/justSomeGithubPractice/justSomeGithubPractice.java
@@ -0,0 +1,12 @@
+package justSomeGithubPractice;
+
+public class justSomeGithubPractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is only for GitHub practice");
+		
+
+	}
+
+}
